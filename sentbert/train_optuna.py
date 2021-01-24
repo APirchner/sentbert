1e8a6da8cad67bd8efbe843efda0dc7662684c37
Andreas Pirchner <andreas.pirchner1990@gmail.com>
2021-01-24 20:18:43 +0100

    updated Optuna tuning

--- sentbert/train_optuna.py
+++ sentbert/train_optuna.py
@@ -10,8 +10,6 @@ from model import SentBert
 
 
 class MetricsCallback(pl.Callback):
-    """PyTorch Lightning metric callback."""
-
     def __init__(self):
         super().__init__()
         self.metrics = []
@@ -21,9 +19,19 @@ class MetricsCallback(pl.Callback):
 
 
 def main(args: Namespace):
+    pruner = optuna.pruners.MedianPruner()
+    study = optuna.create_study(storage=os.path.join('sqlite:///{}'.format(args.log_dir), 'optuna.db'),
+                                study_name=args.study_name,
+                                load_if_exists=True,
+                                direction='minimize', pruner=pruner
+                                )
+
     def objective(trial: optuna.Trial):
+        log_dir = os.path.join(args.log_dir, 'trial_{}'.format(trial.number))
         checkpoint_callback = pl.callbacks.ModelCheckpoint(
-            os.path.join(args.log_dir, 'trial_{}'.format(trial.number), '{epoch}'), monitor='val_ce'
+            dirpath=log_dir,
+            monitor='val_ce',
+            mode='min'
         )
 
         data = SemEvalDataModule(
@@ -45,34 +53,28 @@ def main(args: Namespace):
 
         model = SentBert(out_classes=3, lr_bert=lr_bert, lr_class=lr_class,
                          weight_decay=weight_decay, train_steps=effective_steps)
-
         metrics_callback = MetricsCallback()
         pruning_callback = PyTorchLightningPruningCallback(trial, monitor='val_ce')
         trainer = pl.Trainer.from_argparse_args(
             args,
+            default_root_dir=args.log_dir,
             max_epochs=epochs,
-            checkpoint_callback=checkpoint_callback,
+            checkpoint_callback=True,
             accelerator='ddp',
             auto_select_gpus=True,
-            default_root_dir=args.log_dir,
             num_sanity_val_steps=0,
             profiler='simple',
-            callbacks=[metrics_callback, pruning_callback]
+            callbacks=[checkpoint_callback, metrics_callback, pruning_callback]
         )
         trainer.fit(model=model, datamodule=data)
         return metrics_callback.metrics[-1]['val_ce'].item()
 
-    pruner = optuna.pruners.MedianPruner()
-    study = optuna.create_study(direction='minimize', pruner=pruner)
     study.optimize(objective, n_trials=10, n_jobs=1)
 
     print('Number of finished trials: {}'.format(len(study.trials)))
-
     print('Best trial:')
     trial = study.best_trial
-
     print('  Value: {}'.format(trial.value))
-
     print('  Params: ')
     for key, value in trial.params.items():
         print('    {}: {}'.format(key, value))
@@ -82,6 +84,8 @@ if __name__ == '__main__':
     argparser = ArgumentParser(add_help=True)
     argparser.add_argument('--log_dir', type=str,
                            help='The base log directory')
+    argparser.add_argument('--study_name', type=str,
+                           help='The name of the Optuna study')
     argparser.add_argument('--seed', type=int, default=1234,
                            help='The seed for sampling etc')
     argparser = SemEvalDataModule.add_argparse_args(argparser)
@@ -89,4 +93,5 @@ if __name__ == '__main__':
     argparser = pl.Trainer.add_argparse_args(argparser)
     args = argparser.parse_args()
     pl.seed_everything(args.seed)
+    os.makedirs(args.log_dir, exist_ok=True)
     main(args)
